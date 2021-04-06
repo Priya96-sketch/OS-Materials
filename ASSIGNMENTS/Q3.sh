07d62485cbeae206d3b03ecf3ce46f0427fc6726
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2021-04-06 14:34:48 +0530

    Create Q3.sh

---
+++ ASSIGNMENTS/Q3.sh
@@ -0,0 +1,19 @@
+echo "Enter Size"
+read N
+
+i=1
+sum=0
+
+
+echo "Enter Numbers"
+
+while [ $i -le $N ]
+do
+  read num
+  sum=$((sum + num))
+  i=$((i + 1))
+done
+
+avg=$(echo $sum / $N | bc -l)
+
+echo "Average = $avg"
